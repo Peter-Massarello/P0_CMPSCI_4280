64226fbbf3f4b32d90842a16ee52c217a367e0c1
petermassarello <pmassarello@gmail.com>
2021-09-16 12:24:02 -0500

    add make file

--- main_files/main.cpp
+++ main_files/main.cpp
@@ -2,8 +2,13 @@
 
 using namespace std;
 
-int main(){
-    cout << "Hello World";
+int main(int argc, char **argv){
+    if (argc == 1) {
+        cout << "no arg given\n";
+    } else if (argc == 2) {
+        cout << "1 arg given\n";
+    } else 
+        cout << "ERROR: Too many args given, existing...\n";
 
     return 0;
-}
+} 
